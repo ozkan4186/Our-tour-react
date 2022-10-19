1ba09aed9311b18ce3cfe3a80aed8838bda46acc
özkan demir <ozkanndemir2009@gmail.com>
2022-10-19 12:17:34 +0300

    map done

--- src/Tours.js
+++ src/Tours.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = ({setData,data}) => {
+  // const [item, setİtem] = useState("")
   return(
     <div>
+      {data.map((item)=>{
+        return(
+          <div key={item.id} >
+            <Tour  item={item}/>
+          </div>
+        )
+       
+     
 
+       })}
     </div>
   )
 
