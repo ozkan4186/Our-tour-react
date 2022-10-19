3e3565cbb58581cf72c9b73e0c184588d3be8439
özkan demir <ozkanndemir2009@gmail.com>
2022-10-19 11:21:27 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+
+
+
+
+  
+  return 
+  <h2>
+  
+
+  </h2>
+}
+
+export default App
