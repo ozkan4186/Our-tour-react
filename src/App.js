07825ca31d28d5011d9090c857e756e149b91696
özkan demir <ozkanndemir2009@gmail.com>
2022-10-19 12:00:19 +0300

    third

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 // I SWITCHED TO PERMANENT DOMAIN
-
+const url = "https://course-api.com/react-tours-project";
 function App() {
-const url = 'https://course-api.com/react-tours-project'
+  const [data, setData] = useState([]);
 
+  const axiosTours = async () => {
+    try {
+      const response = await axios(url);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    axiosTours();
+  }, []);
 
+  console.log(data);
 
-  return 
-  <h2>
-  
-
-  </h2>
+  return(
+    <div>
+      <Tours setData={setData}  data={data}/>
+    </div>
+  )
 }
 
-export default App
+export default App;
